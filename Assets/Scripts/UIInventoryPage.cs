565cce4d122acb42bfbda58bc19107c334515711
JarretL413 <31018726+JarretL413@users.noreply.github.com>
2024-11-08 17:39:46 -0900

    Almost Done with inventory :)

---
+++ Assets/Scripts/UIInventoryPage.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIInventoryPage : MonoBehaviour
+{
+    [SerializeField] private UIInventoryItem itemPrefab;
+
+    [SerializeField] private RectTransform contentPanel;
+
+    List<UIInventoryItem> listOfUIItems = new List<UIInventoryItem>();
+
+    public void InitializeInventoryUI(int inventorysize)
+    {
+        for (int i = 0; i < inventorysize; i++)
+        {
+            UIInventoryItem uiItem = Instantiate(itemPrefab, Vector3.zero, Quaternion.identity);
+            uiItem.transform.SetParent(contentPanel);
+            uiItem.transform.localScale = new Vector3(1, 1, 1);
+            listOfUIItems.Add(uiItem);
+            uiItem.OnItemClicked += HandleItemSelection;
+            uiItem.OnItemBeginDrag += HandleBeginDrag;
+            uiItem.OnItemDroppedOn += HandleSwap;
+            uiItem.OnItemEndDrag += HandleEndDrag;
+            uiItem.OnRightMouseBtnClick += HandleShowItemActions;
+        }
+    }
+
+    private void HandleShowItemActions(UIInventoryItem obj)
+    {
+        
+    }
+
+    private void HandleEndDrag(UIInventoryItem obj)
+    {
+       
+    }
+
+    private void HandleSwap(UIInventoryItem obj)
+    {
+        
+    }
+
+    private void HandleBeginDrag(UIInventoryItem obj)
+    {
+        
+    }
+
+    private void HandleItemSelection(UIInventoryItem obj)
+    {
+        Debug.Log(obj.name);
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive (true);
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+
+}
