9f2f1c4d91c00f5d9c6d2b0803fedd24c2c4c791
JarretL413 <31018726+JarretL413@users.noreply.github.com>
2024-11-20 23:31:02 -0900

    Pretty close to the end
    its functioning but ill need to do the action buttons to use the items. I'll probably do some on Saturday...... I need a break lol

--- Assets/Scripts/HealthController.cs
+++ Assets/Scripts/HealthController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.Android.LowLevel;
 using UnityEngine.UI;
 
 public class HealthController : MonoBehaviour
@@ -8,55 +9,49 @@ public class HealthController : MonoBehaviour
     private float _maxHealth = 100;
     private float _currentHealth;
     [SerializeField] private Image _healthBarFill;
-    [SerializeField] private PlayerController _playerController;
-    [SerializeField] private float _damageAmount,_sanityAmount;
+    [SerializeField] private float _damageAmount,_healthAmount;
     [SerializeField] private Transform _healthBarTransform;
     private Camera _camera;
-
-
-
     private void Awake()
     {
         _currentHealth = _maxHealth;
         _camera = Camera.main;
     }
-
     private void Update()
     {
         _healthBarTransform.rotation = _camera.transform.rotation;
     }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Enemy"))
+        if(collision.CompareTag("Enemy"))
         {
             TakeDamage(_damageAmount);
         }
         else if (collision.CompareTag("Sanity"))
         {
-            Sanity(_sanityAmount);
-            collision.gameObject.SetActive(false);
+            Heal(_healthAmount);
         }
-    }
-    private void TakeDamage(float amount)
-    {
-        _currentHealth -= amount;
-        _currentHealth = Mathf.Clamp(_currentHealth, 0, _maxHealth);
-        if (_currentHealth == 0)
-        {
-            _currentHealth = _maxHealth;
-        }
-        UpdateHealthBar();
     }
 
-    private void Sanity(float amount)
+    private void Heal(float amount)
     {
         _currentHealth += amount;
         _currentHealth = Mathf.Clamp(_currentHealth, 0, _maxHealth);
         UpdateHealthBar();
     }
+    private void TakeDamage(float amount)
+    {
+        _currentHealth -= amount;   
+        _currentHealth = Mathf.Clamp(_currentHealth, 0, _maxHealth);
+        if (_currentHealth == 0 )
+        {
+            
+            _currentHealth = _maxHealth;
+        }
+        UpdateHealthBar();
+    }
     private void UpdateHealthBar()
     {
-        _healthBarFill.fillAmount = _currentHealth / _maxHealth;
+        _healthBarFill.fillAmount = _currentHealth/_maxHealth;
     }
 }
--- Assets/Scripts/InventoryController.cs
+++ Assets/Scripts/InventoryController.cs
@@ -1,31 +1,112 @@
+using Inventory.Model;
+using Inventory.UI;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 
-public class InventoryController : MonoBehaviour
+namespace Inventory
 {
-    [SerializeField] private UIInventoryPage inventoryUI;
+    public class InventoryController : MonoBehaviour
+    {
+        [SerializeField] private UIInventoryPage inventoryUI;
 
-    public int inventorySize = 10;
-    private void Start()
-    {
-        inventoryUI.InitializeInventoryUI(inventorySize);
-    }
-    public void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.R))
+        [SerializeField] private InventorySO inventoryData;
+
+        public List<InventoryItem> initialItems = new List<InventoryItem>();    
+        private void Start()
         {
-            if (inventoryUI.isActiveAndEnabled == false)
+            PrepareUI();
+            PrepareInventoryData();
+        }
+
+        private void PrepareInventoryData()
+        {
+            inventoryData.Initialize();
+            inventoryData.OnInventoryUpdated += UpdateInventoryUI;
+            foreach (InventoryItem item in initialItems)
             {
-                inventoryUI.Show();
-               
+                if (item.IsEmpty)
+                    continue;
+                inventoryData.AddItem(item);
+            }
+        }
+
+        private void UpdateInventoryUI(Dictionary<int, InventoryItem> inventoryState)
+        {
+            inventoryUI.ResetAllItems();
+            foreach (var item in inventoryState)
+            {
+                inventoryUI.UpdateData(item.Key, item.Value.item.ItemImage,
+                    item.Value.quantity);
+            }
+        }
+
+        private void PrepareUI()
+        {
+            inventoryUI.InitializeInventoryUI(inventoryData.Size);
+            inventoryUI.OnDescriptionRequested += HandleDescriptionRequest;
+            inventoryUI.OnSwapItems += HandleSwapItems;
+            inventoryUI.OnStartDragging += HandleDragging;
+            inventoryUI.OnItemActionRequested += HandleItemActionRequest;
+        }
+
+        private void HandleItemActionRequest(int itemIndex)
+        {
+
+        }
+
+        private void HandleDragging(int itemIndex)
+        {
+            InventoryItem inventoryItem = inventoryData.GetItemAt(itemIndex);
+            if (inventoryItem.IsEmpty)
+                return;
+            inventoryUI.CreateDraggedItem(inventoryItem.item.ItemImage, inventoryItem.quantity);
+        }
+
+        private void HandleSwapItems(int itemIndex_1, int itemIndex_2)
+        {
+            inventoryData.SwapItems(itemIndex_1, itemIndex_2);
+        }
+
+        private void HandleDescriptionRequest(int itemIndex)
+        {
+            InventoryItem inventoryItem = inventoryData.GetItemAt(itemIndex);
+            if (inventoryItem.IsEmpty)
+            {
+                inventoryUI.ResetSelection();
+                return;
+            }
+            ItemSO item = inventoryItem.item;
+            inventoryUI.UpdateDescription(itemIndex, item.ItemImage,
+                item.name, item.Description);
+
+
+
+        }
+
+        public void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                if (inventoryUI.isActiveAndEnabled == false)
+                {
+                    inventoryUI.Show();
+                    foreach (var item in inventoryData.GetCurrentInventoryState())
+                    {
+                        inventoryUI.UpdateData(item.Key,
+                            item.Value.item.ItemImage,
+                            item.Value.quantity);
+                    }
+
+                }
+                else
+                {
+                    inventoryUI.Hide();
+                }
 
             }
-            else
-            {
-                inventoryUI.Hide();
-            }
-
         }
     }
 }
