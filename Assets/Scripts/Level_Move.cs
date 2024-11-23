da7a4ab40ab4b49d1d1b3109073e1d9096a16d39
pmratzer <pmratzer@alaska.edu>
2024-11-23 13:07:06 -0900

    scenes/transitions
    altered some scenes, created some new ones, added some downloaded tiles, and got the first scene transition done

---
+++ Assets/Scripts/Level_Move.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level_Move : MonoBehaviour
+{
+    public int sceneBuildIndex;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Trigger Zone Entered");
+
+        if(other.tag == "Player")
+        {
+            Debug.Log("Entering Scene " + sceneBuildIndex);
+            SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
+        }
+    }
+}
